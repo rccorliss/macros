58fd2856566c9175b5afcf8cd58af467577cad33
Ross Corliss <rcorliss@mit.edu>
2021-06-01 04:19:58 -0400

    changed globalvariables min verbosity=1001

--- common/GlobalVariables.C
+++ common/GlobalVariables.C
@@ -32,7 +32,7 @@ namespace Enable
   bool DSTOUT_COMPRESS = false;
   bool OVERLAPCHECK = false;
   bool SUPPORT = false;
-  int VERBOSITY = 10;
+  int VERBOSITY = 1001;
 }  // namespace Enable
 
 // every G4 subsystem needs to implement this
