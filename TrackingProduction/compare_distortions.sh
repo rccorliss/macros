16e4e648c3a3d2fce646b1dcdaaa73148bd902cf
Ross Corliss <rcorliss@mit.edu>
2024-05-27 23:24:54 -0400

    shell script to do the iteration

--- TrackingProduction/compare_distortions.sh
+++ TrackingProduction/compare_distortions.sh
@@ -4,7 +4,7 @@ dists=`ls static*.root`
 
 for file in $dists
 do
-    root -l Fun4All_FieldOnAllTrackers.C\\\(100,\\\"${file}\\\"\\\)
+    root -l Fun4All_FieldOnAllTrackers.C\(100,\"${file}\"\)
     mkdir ${file}.output
     mv cluster*  ${file}.output
     mv trkrntuple.root  ${file}.output
